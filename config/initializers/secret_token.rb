7fd87f7a09ab97a70314ba887c322bb2a767ca4f
Jeff Higham <jeffhigham@gmail.com>
2013-07-24 12:20:45 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flash360::Application.config.secret_token = '8a13497b6918f48810cf42eeeff489273e84d246a896d2d5624429d7405ea7960965b17dc14500d8e00e989924bf9b3b0b7cb39adec0c248dc12dbb43a91c8a3'
